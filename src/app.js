59e126ede6e3ba66b3c2fc03e9a5105be0d59ce0
Nurullo <karimovdeveloper@gmail.com>
2023-06-19 17:39:42 +0500

    New commit

--- src/app.js
+++ src/app.js
@@ -3,8 +3,11 @@ import React from "react";
 export default class App extends React.Component {
   render() {
     return (
-      <div className="flex justify-center items-center h-[100vh]">
-        <h1 className=" text-white font-pally text-[100px]">Hello world!</h1>
+      <div className="flex justify-center items-center h-[100vh] flex-col">
+        <h1 className=" text-white font-pally text-[100px] block">Hello world!</h1>
+        <a href="https://www.nurullokarimov.uz/" className="font-ego tracking-widest hover:text-white underline">
+          nurullokarimov.uz
+        </a>
       </div>
     );
   }
